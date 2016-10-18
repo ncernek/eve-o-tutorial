578536dda7f0249f374ee6679592584aa0d168f8
ncernek <ncernek@gmail.com>
2016-10-17 21:39:03 -0700

    changed import of module to proper name

--- app/components/App.js
+++ app/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Footer from './footer';
-import Navbar from './navbar';
+import Footer from './Footer';
+import Navbar from './Navbar';
 
 class App extends React.Component {
   render() {
